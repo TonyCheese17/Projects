1ee35f29a02d0cf9ed8893fdb13709d6d277d542
TonyCheese11 <wyu@ideaventionsacadey.org>
2017-11-28 12:08:08 -0500

    1

---
+++ game_show.py
@@ -0,0 +1,25 @@
+# Tony Yu
+# 11/28/17
+# GET A NEW CAR NOW
+
+import time
+
+count = 0
+count2 = 0
+
+letter = "$"
+letter2 = "$"
+
+while count <= 35:
+    print(letter,end='')
+    time.sleep(0.1)
+    count = count + 1
+
+print()
+
+print("WELCOME TO THE FATSIE GAME SHOW!!!")
+
+while count2 <= 35:
+    print(letter2,end='')
+    time.sleep(0.1)
+    count2 = count2 + 1
